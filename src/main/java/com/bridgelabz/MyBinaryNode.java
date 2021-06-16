542026bc47b04c9c79339b19574a67568829ffe4
shwetap29 <shwetaprasad48@gmail.com>
2021-06-17 00:18:14 +0530

    Welcome To Binary Search Tree

---
+++ src/main/java/com/bridgelabz/MyBinaryNode.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class MyBinaryNode<K> {
+    public MyBinaryNode<K> left;
+    public MyBinaryNode<K> right;
+    public K key;
+
+    public MyBinaryNode(K key) {
+    }
+}
